581f666d4a84f965446fca6b85993fd7e2153e15
mmartine128 <mmartine128@cuc.edu.co>
2024-10-02 14:35:12 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import milogo from './assets/img/flash.jpg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={milogo} alt="Logo de the flash" />
+        <h1> !! The Flash !! </h1>
+        <p> Es un superhéroe que aparece en los cómics estadounidenses publicados por DC Comics. Barry Allen Es el segundo personaje conocido como Flash, siguiendo a Jay Garrick.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
